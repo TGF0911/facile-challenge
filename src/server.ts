54175705cdb3640a704b11b6177ca037bbf6c73f
Thais Gennari <54035011+TGF0911@users.noreply.github.com>
2021-12-21 18:36:05 -0300

    Adicionando configurações do heroku

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,11 @@
 import express from 'express';
 import { router } from './routes';
 import './database';
+const port = process.env.PORT || 3333;
 
 const app = express();
 
 app.use(express.json());
 app.use(router);
 
-app.listen(process.env.PORT || 3333, () => console.log(`Server running on port 3333 or ${process.env.PORT}`));
+app.listen(port, () => console.log(`Server running on port 3333 or ${process.env.PORT}`));
