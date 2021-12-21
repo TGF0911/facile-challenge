8e412b05985c50a6adc4f69d8f8ce3420e3fa135
Thais Gennari <54035011+TGF0911@users.noreply.github.com>
2021-12-21 18:25:03 -0300

    Adicionando variáveis no .env

--- src/server.ts
+++ src/server.ts
@@ -7,4 +7,4 @@ const app = express();
 app.use(express.json());
 app.use(router);
 
-app.listen(3333, () => console.log('Server running on port 3333'));
+app.listen(process.env.PORT || 3333, () => console.log(`Server running on port 3333 or ${process.env.PORT}`));
